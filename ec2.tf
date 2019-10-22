b1dea793c28e9ac3d66eb979d9936c723ff7aa92
M. Holger <matt@paralleltheory.com>
2019-10-22 17:44:20 -0500

    Adding EC2 ASG definitions

---
+++ ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_launch_configuration" "ecs_launch" {
+  name                 = "${var.project_name}-${terraform.workspace}-ecs_launch_group"
+  image_id             = "${var.ecs_ami}"
+  instance_type        = "${var.ecs_instance_type}"
+  iam_instance_profile = "${aws_iam_instance_profile.ecs_instance.id}"
+
+  root_block_device {
+    volume_type           = "standard"
+    volume_size           = "${var.ecs_root_size}"
+    delete_on_termination = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  security_groups             = ["${aws_security_group.ecs_public.id}"]
+  associate_public_ip_address = false
+  key_name                    = "${var.ecs_key_pair_name}"
+  user_data                   = <<EOF
+    #!/bin/bash
+    echo ECS_CLUSTER=${var.project_name}-${terraform.workspace} >> /etc/ecs/ecs.config
+    EOF
+}
+
+resource "aws_autoscaling_group" "ecs_asg" {
+  name                 = "${var.project_name}-${terraform.workspace}-asg"
+  max_size             = "${var.ecs_max_size}"
+  min_size             = "${var.ecs_min_size}"
+  desired_capacity     = "${var.ecs_desired_size}"
+  vpc_zone_identifier  = var.private_subnets
+  launch_configuration = "${aws_launch_configuration.ecs_launch.name}"
+  health_check_type    = "ELB"
+}
--- variables.tf
+++ variables.tf
@@ -19,3 +19,49 @@ variable "public_subnet" {
   type        = string
   description = "Public subnet ID from tf_vpc module"
 }
+
+variable "private_subnets" {
+  type        = list
+  description = "List of Private subnet IDs from tf_vpc module, within which ECS instances will be placed"
+}
+
+variable "ecs_instance_type" {
+  type        = string
+  description = "AWS EC2 instance type for ECS cluster nodes; default: t3.large"
+  default     = "t3.large"
+}
+
+variable "ecs_ami" {
+  type        = string
+  description = "AWS AMI ID for ECS cluster nodes"
+  default     = "ami-..."
+}
+
+variable "ecs_root_size" {
+  type        = string
+  description = "Size of ECS instance root volume in GB; default: 100G"
+  default     = 100
+}
+
+variable "ecs_key_pair_name" {
+  type        = string
+  description = "Name of AWS EC2 key pair for SSH authentication"
+}
+
+variable "ecs_min_size" {
+  type        = string
+  description = "Minimum number of ECS instances within the cluster; default: 1"
+  default     = 1
+}
+
+variable "ecs_max_size" {
+  type        = string
+  description = "Maximum number of ECS instances within the cluster; default: 1"
+  default     = 1
+}
+
+variable "ecs_desired_size" {
+  type        = string
+  description = "Preferred number of ECS instances within the cluster -- should be between ecs_min_size and ecs_max_size, inclusive; default: 1"
+  default     = 1
+}
